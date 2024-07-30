7730ac612d851e30add8d87987dc4ff21a98510b
Shailesh Saravanan <77380733+Shaileshsaravanan@users.noreply.github.com>
2024-07-30 21:09:38 +0530

    basic ui

--- api/index.py
+++ api/index.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/upcoming')
+def upcoming():
+    return render_template('upcoming.html')
+    
+@app.route('/about')
+def about():
+    return 'About'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
