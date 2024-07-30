aaf837ce60ea8ee73d39817b5d0aaa4725a55c44
Joel Joby <joeljobyp@gmail.com>
2024-07-30 21:09:59 +0530

    updated

--- api/index.py
+++ api/index.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+#internal API
+@app.route('/api/v1/login_ver', methods=['POST'])
+def Verify_Login():
+    login_credentials = request.json()
+    user_email = login_credentials['email']
+    user_password = login_credentials['password']
+    return 'Login'
+if __name__ == '__main__':
+    app.run(debug=True)
