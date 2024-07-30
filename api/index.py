8ba97d23761493cb8569f9bb36d5ceb645bbce1f
Shailesh Saravanan <77380733+Shaileshsaravanan@users.noreply.github.com>
2024-07-30 22:40:35 +0530

    more ui stuff

--- api/index.py
+++ api/index.py
@@ -1,22 +1,25 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template
 from dotenv import load_dotenv
+
 load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/upcoming')
+def upcoming():
+    return render_template('upcoming.html')
+    
+@app.route('/programs')
+def programs():
+    return render_template('programs.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-#internal API
-@app.route('/api/v1/login_ver', methods=['POST'])
-def Verify_Login():
-    login_credentials = request.json()
-    user_email = login_credentials['email']
-    user_password = login_credentials['password']
-    return 'Login'
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
